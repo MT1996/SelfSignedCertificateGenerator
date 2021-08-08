27271cd9400eb236e2ceb0879ea74c0f416382b0
marc <Marc.Totzke@t-online.de>
2021-08-08 10:07:56 +0200

    Generating CA-Function Completed

--- com/SelfOneDrive/WebmanagementSystem/CertificateGenerator/SelfSignedCertificateGenerator.py
+++ com/SelfOneDrive/WebmanagementSystem/CertificateGenerator/SelfSignedCertificateGenerator.py
@@ -1,43 +1,71 @@
 import datetime
 from typing import List, Tuple
+
+from builtins import classmethod
 from cryptography import x509
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.x509.base import Certificate, CertificateSigningRequest
+from cryptography.x509.base import Certificate, CertificateSigningRequest, CertificateBuilder
 from cryptography.x509.extensions import ExtensionType
 from cryptography.x509.name import NameAttribute
+
 
 class SelfSignedCertificateGenerator:
 
     def __init__(self) -> None:
         pass
 
-    def generate_Certificate_Authority(
-        privateKey: rsa.RSAPrivateKey,
-        ca_information: List[x509.NameAttribute], 
-        not_valid_before: datetime.datetime, 
-        not_valid_after: datetime.datetime,
-        serial_number: int,
-        extensions: List[ExtensionType]
+    @classmethod
+    def generate_Certificate_Authority(cls,
+       privateKey: rsa.RSAPrivateKey,
+       ca_information: List[x509.NameAttribute],
+       not_valid_before: datetime.datetime,
+       not_valid_after: datetime.datetime,
+       serial_number: int,
+       extension: ExtensionType
     ) -> Tuple[Certificate, rsa.RSAPrivateKey]:
-        pass
+        publicKey = privateKey.public_key()
+        certificate = CertificateBuilder().subject_name(
+            x509.Name(ca_information)
+        ).issuer_name(
+            x509.Name(ca_information)
+        ).not_valid_before(
+            not_valid_before
+        ).not_valid_after(
+            not_valid_after
+        ).serial_number(
+            serial_number
+        ).public_key(
+            publicKey
+        ).add_extension(
+            extension
+        ).sign(privateKey, hashes.SHA256())
 
-    def generate_PrivateKey(public_exponent: int, key_size: int) -> rsa.RSAPrivateKey:
+        return ()
+
+    @classmethod
+    def generate_PrivateKey(cls, public_exponent: int, key_size: int) -> rsa.RSAPrivateKey:
         return rsa.generate_private_key(
             public_exponent,
             key_size
         )
 
-    def get_PublicKey_from_PrivateKey(privateKey: rsa.RSAPrivateKey) -> rsa.RSAPublicKey:
+    @classmethod
+    def get_PublicKey_from_PrivateKey(cls, privateKey: rsa.RSAPrivateKey) -> rsa.RSAPublicKey:
         return privateKey.public_key()
 
+    @classmethod
     def generate_CertificationSigningRequest(
+        cls,
         privateKey: rsa.RSAPrivateKey,
         csr_information: List[NameAttribute],
         additional_extensions: List[ExtensionType]
     ) -> CertificateSigningRequest:
         pass
 
+    @classmethod
     def generate_SelfSignedServerCertificate(
+        cls,
         csr: CertificateSigningRequest, 
         rootCACrt: Certificate, 
         rootCAKey: rsa.RSAPrivateKey
--- com/SelfOneDrive/WebmanagementSystem/CertificateGenerator/main.py
+++ com/SelfOneDrive/WebmanagementSystem/CertificateGenerator/main.py
@@ -1,14 +1,15 @@
-from typing import List, Tuple
 from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.hazmat.backends import default_backend
-from cryptography import x509
-from cryptography.hazmat.primitives.serialization.base import load_pem_private_key
-from cryptography.x509.base import Certificate, CertificateBuilder, CertificateSigningRequest, load_pem_x509_certificate, load_pem_x509_csr
-from cryptography.x509.extensions import ExtensionType
-from cryptography.x509.name import NameAttribute
-from cryptography.x509.oid import NameOID
 import datetime
+
+from cryptography import x509
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives.serialization.base import load_pem_private_key
+from cryptography.x509.base import Certificate, CertificateBuilder, CertificateSigningRequest, \
+    load_pem_x509_certificate, load_pem_x509_csr
+from cryptography.x509.oid import NameOID
+
 
 def generate_CA():
     public_exponent = 65537
